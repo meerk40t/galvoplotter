e79dd7c8c46d3d0a1e738f079176120264ac4011
Tatarize <tatarize@gmail.com>
2023-07-05 13:16:50 -0700

    Add generator test job for execution

--- test/test_api.py
+++ test/test_api.py
@@ -1,20 +1,50 @@
 import time
 import unittest
+from galvo import GalvoController
 
 state = 0
 
 
 class TestAPI(unittest.TestCase):
-    def test_api(self):
-        from galvo import GalvoController
+    def test_api_job(self):
+        """
+        Test submission of a test job.
 
+        Test passes if the job quits out at around ~2 seconds.
+        :return:
+        """
         controller = GalvoController(settings_file="test.json")
 
-        def my_command():
+        def my_job():
+            """
+            Never indicates job is finished.
+            :return:
+            """
+            controller.light_mode()
             controller.dark(0x8000, 0x8000)
             controller.light(0x2000, 0x2000)
             return False
 
-        controller.submit(my_command)
+        controller.submit(my_job)
+        time.sleep(2)
+        controller.shutdown()
+
+    def test_api_generator(self):
+        """
+        Test submission of a generator test job.
+
+        Test passes if the job quits out at around ~2 seconds.
+
+        :return:
+        """
+
+        controller = GalvoController(settings_file="test.json")
+
+        def my_generator():
+            while True:
+                yield "dark", 0x8000, 0x8000
+                yield "light", 0x2000, 0x2000
+
+        controller.submit(controller.generate_job(my_generator))
         time.sleep(2)
         controller.shutdown()
